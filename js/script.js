ef23e0130eb7fc42a99a2a332841a1ed91c00b36
ElisaMavilia <elisamavilia1@gmail.com>
2024-03-15 14:48:29 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/* L'utente clicca su un bottone che genererà una griglia di gioco quadrata. 
+Ogni cella ha un numero progressivo, da 1 a 100. 
+Ci saranno quindi 10 caselle per ognuna delle 10 righe. 
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+ */
+
+// COSA MI SERVE?
+
+// - Una funzione event listener per il bottone (per far apparire la griglia)
+// - Un ciclo for per duplicare i singoli box
+// - Una funzione per il clic dei singoli box
+// - Condizione per la stampa in console 
